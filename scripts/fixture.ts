53ac3ab0eecc618c828afaf6cd289cc30ce5085c
JohnYangMMHC <johnyang@mhhc.com.au>
2024-06-05 09:14:29 +0800

    bug fix

---
+++ scripts/fixture.ts
@@ -0,0 +1,12 @@
+export interface UsePoolsQuery<TData> extends ReactQueryParams<QueryPoolsResponse, TData> {
+    request?: QueryPoolsRequest;
+}
+const usePools = <TData = QueryPoolsResponse,>({
+    request,
+    options
+}: UsePoolsQuery<TData>) => {
+    return useQuery<QueryPoolsResponse, Error, TData>(["poolsQuery", request], () => {
+        if (!queryService) throw new Error("Query Service not initialized");
+        return queryService.pools(request);
+    }, options);
+};
